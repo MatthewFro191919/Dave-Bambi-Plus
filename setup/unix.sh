fa1ddb59e8184aff3da9dc2f001580e864cc9244
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-02 19:03:34 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,8 +7,8 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.0.0
-haxelib install openfl 9.3.3
+haxelib install lime 7.9.0
+haxelib install openfl 9.1.0
 haxelib install flixel 4.11.0
 haxelib install flixel-addons 2.9.0
 haxelib install flixel-tools 1.5.1
