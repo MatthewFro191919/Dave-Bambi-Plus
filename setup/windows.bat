5545b9e0118a22b2590a0f952c6b0a9fa2adb248
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-02 16:51:47 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -12,6 +12,7 @@ haxelib install flixel-tools 1.5.1
 haxelib install flixel-ui
 haxelib install hxcpp
 haxelib install hxCodec 2.5.1
+haxelib install hscript
 haxelib install hxdiscord_rpc 2.5.1
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
